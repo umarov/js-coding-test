d148f8beefd9edeede160686a3303d04b261371f
Muzafar Umarov <umarov@users.noreply.github.com>
2022-02-02 10:52:38 -0500

    Add step 1 tests

--- src/step-1.spec.js
+++ src/step-1.spec.js
@@ -1,7 +1,31 @@
-import { VendorMachine } from "./vending-machine";
+import { VendingMachine } from "./vending-machine";
 
 describe("Vending Machine Step 1", () => {
-  it("initializes", () => {
-    expect(new VendorMachine(1, 2)).toBeDefined();
+  const rows = Math.floor(Math.random() * 10);
+  const columns = Math.floor(Math.random() * 10);
+  const machine = new VendingMachine(rows, columns);
+
+  it("is defined", () => {
+    expect(machine).toBeDefined();
+  });
+
+  describe("dimensions", () => {
+    it("has the correct number of rows", () => {
+      expect(machine.rows).toEqual(rows);
+    });
+
+    it("has the correct number of columns", () => {
+      expect(machine.columns).toEqual(columns);
+    });
+
+    it("has the correct capacity", () => {
+      expect(machine.capacity).toEqual(rows + columns);
+    });
+  });
+
+  describe("stock", () => {
+    it("shows the correct value for in stock", () => {
+      expect(machine.inStock()).toEqual(0);
+    });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,1 +1,1 @@
-export class VendorMachine {}
+export class VendingMachine {}
